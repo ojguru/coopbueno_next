a10bf5ba3ddde5da0eb7405a54089cf0a1398fa5
Osiris Peña <osiris.pena@conuco.do>
2022-07-25 22:48:49 -0400

    servidores permitidos para imagenes actualizados

--- next.config.js
+++ next.config.js
@@ -6,8 +6,9 @@ const nextConfig = {
       // "[yourapp].wpengine.com" (Update this to be your Wordpress application name in order to load images connected to your posts)
       "localhost",
       "certificaciones.uaf.gob.do",
-      "coopbueno.herokuapp.com",
       "coopbueno.netlify.app",
+      "coopbueno.com.do",
+      "app.coopbueno.com.do",
       "res.cloudinary.com",
     ],
   },
