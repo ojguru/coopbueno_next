ef081baaa90202e4bc8bae9c2726361b91875000
Osiris Peña <osiris.pena@conuco.do>
2022-07-26 00:33:02 -0400

    lang atributo en espanol para todo el contenido

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,9 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="es">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -17,8 +17,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
