6fbed35200d409a0638b931d35467e0c87085286
Osiris Peña <osiris.pena@conuco.do>
2022-07-28 18:14:00 -0400

    enviando metricas wpo a analitycs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,25 @@
 import { Global } from "@emotion/react";
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps, NextWebVitalsMetric } from "next/app";
 import globalStyle from "styles/global-styles";
 import { AppWrapper } from "context/appContext";
 import { NextSeo } from "next-seo";
 import Head from "next/head";
 import { SITE_NAME, SITE_URL } from "lib/constants";
 import { useRouter } from "next/router";
-import { GoogleAnalytics, usePageViews } from "nextjs-google-analytics";
+import { GoogleAnalytics, usePageViews, event } from "nextjs-google-analytics";
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  event(metric.name, {
+    category:
+      metric.label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+    value: Math.round(
+      metric.name === "CLS" ? metric.value * 1000 : metric.value
+    ), // values must be integers
+    label: metric.id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  });
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
