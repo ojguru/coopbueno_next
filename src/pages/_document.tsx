fe160a727d59dfed7b363289db491b0aa1ff73b9
Osiris Peña <osiris.pena@conuco.do>
2022-08-04 23:07:15 -0400

    probando worker con conversaciones

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,12 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          {/* <link
+            rel="preload"
+            href="https://js.usemessages.com/conversations-embed.js"
+            as="script"
+          /> */}
+
           {/* <link rel="preload" href="/conversation.js" as="script" /> */}
 
           <link
@@ -68,67 +74,138 @@ class MyDocument extends Document {
               `}
           </Script>
 
-          <Script
-            type="text/javascript"
-            id="hs-script-loader"
-            async
-            defer
-            src="//js.hs-scripts.com/5494710.js"
-            strategy="afterInteractive"
-          />
-
           {/* <Script
-            type="text/javascript"
+            id="hubspot-messages-loader"
+            // data-loader="hs-scriptloader"
+            // data-hsjs-portal="5494710"
+            // data-hsjs-env="prod"
+            // data-hsjs-hublet="na1"
+            src="conversation.js"
             async
             defer
-            src="https://js.hs-analytics.net/analytics/1659628500000/5494710.js"
             strategy="worker"
           /> */}
 
-          {/* <Script
-            type="text/javascript"
+          {/* <script
+            id="hubspot-messages-loader"
+            data-loader="hs-scriptloader"
+            data-hsjs-portal="5494710"
+            data-hsjs-env="prod"
+            data-hsjs-hublet="na1"
             async
             defer
-            src="https://js.hs-banner.com/5494710.js"
-            strategy="worker"
           /> */}
 
-          {/* <Script
-            type="text/javascript"
-            async
-            defer
-            src="http://js.hs-scripts.com/5494710.js"
-            strategy="worker"
-          /> */}
-
-          {/* <Script
-            type="text/javascript"
-            async
-            defer
-            src="https://js.hsadspixel.net/fb.js"
-            strategy="worker"
-          /> */}
-          {/* <Script
-            type="text/javascript"
-            async
-            defer
-            src="https://js.hscollectedforms.net/collectedforms.js"
-            strategy="worker"
-          /> */}
-          {/* <Script
-            type="text/javascript"
-            async
-            defer
-            src="https://js.hsleadflows.net/leadflows.js"
-            strategy="worker"
-          /> */}
-          {/* <Script
-            type="text/javascript"
-            async
-            defer
-            src="https://js.usemessages.com/conversations-embed.js"
-            strategy="worker"
-          /> */}
+          <Script id="loader" strategy="afterInteractive">{`
+            !function(t, e, r) {
+              if (!document.getElementById(t)) {
+                  var n = document.createElement("script");
+                  for (var a in n.src = "https://js.usemessages.com/conversations-embed.js",
+                  n.type = "text/javascript",
+                  n.id = t,
+                  r)
+                      r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
+                  var i = document.getElementsByTagName("script")[0];
+                  i.parentNode.insertBefore(n, i)
+              }
+          }("hubspot-messages-loader", 0, {
+              "data-loader": "hs-scriptloader",
+              "data-hsjs-portal": 5494710,
+              "data-hsjs-env": "prod",
+              "data-hsjs-hublet": "na1"
+          });
+          // !function(t, e, r) {
+          //     if (!document.getElementById(t)) {
+          //         var n = document.createElement("script");
+          //         for (var a in n.src = "https://js.hsleadflows.net/leadflows.js",
+          //         n.type = "text/javascript",
+          //         n.id = t,
+          //         r)
+          //             r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
+          //         var i = document.getElementsByTagName("script")[0];
+          //         i.parentNode.insertBefore(n, i)
+          //     }
+          // }("LeadFlows-5494710", 0, {
+          //     "crossorigin": "anonymous",
+          //     "data-leadin-portal-id": 5494710,
+          //     "data-leadin-env": "prod",
+          //     "data-loader": "hs-scriptloader",
+          //     "data-hsjs-portal": 5494710,
+          //     "data-hsjs-env": "prod",
+          //     "data-hsjs-hublet": "na1"
+          // });
+          // !function(t, e, r) {
+          //     if (!document.getElementById(t)) {
+          //         var n = document.createElement("script");
+          //         for (var a in n.src = "https://js.hscollectedforms.net/collectedforms.js",
+          //         n.type = "text/javascript",
+          //         n.id = t,
+          //         r)
+          //             r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
+          //         var i = document.getElementsByTagName("script")[0];
+          //         i.parentNode.insertBefore(n, i)
+          //     }
+          // }("CollectedForms-5494710", 0, {
+          //     "crossorigin": "anonymous",
+          //     "data-leadin-portal-id": 5494710,
+          //     "data-leadin-env": "prod",
+          //     "data-loader": "hs-scriptloader",
+          //     "data-hsjs-portal": 5494710,
+          //     "data-hsjs-env": "prod",
+          //     "data-hsjs-hublet": "na1"
+          // });
+          // var _hsp = window._hsp = window._hsp || [];
+          // _hsp.push(['addEnabledFeatureGates', ["CookieBanner:DomainCollection"]]);
+          // !function(t, e, r) {
+          //     if (!document.getElementById(t)) {
+          //         var n = document.createElement("script");
+          //         for (var a in n.src = "https://js.hs-banner.com/5494710.js",
+          //         n.type = "text/javascript",
+          //         n.id = t,
+          //         r)
+          //             r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
+          //         var i = document.getElementsByTagName("script")[0];
+          //         i.parentNode.insertBefore(n, i)
+          //     }
+          // }("cookieBanner-5494710", 0, {
+          //     "data-cookieconsent": "ignore",
+          //     "data-hs-ignore": true,
+          //     "data-loader": "hs-scriptloader",
+          //     "data-hsjs-portal": 5494710,
+          //     "data-hsjs-env": "prod",
+          //     "data-hsjs-hublet": "na1"
+          // });
+          // !function(e, t) {
+          //     if (!document.getElementById(e)) {
+          //         var c = document.createElement("script");
+          //         c.src = "https://js.hs-analytics.net/analytics/1659664200000/5494710.js",
+          //         c.type = "text/javascript",
+          //         c.id = e;
+          //         var n = document.getElementsByTagName("script")[0];
+          //         n.parentNode.insertBefore(c, n)
+          //     }
+          // }("hs-analytics");
+          // !function(t, e, r) {
+          //     if (!document.getElementById(t)) {
+          //         var n = document.createElement("script");
+          //         for (var a in n.src = "https://js.hsadspixel.net/fb.js",
+          //         n.type = "text/javascript",
+          //         n.id = t,
+          //         r)
+          //             r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
+          //         var i = document.getElementsByTagName("script")[0];
+          //         i.parentNode.insertBefore(n, i)
+          //     }
+          // }("hs-ads-pixel-5494710", 0, {
+          //     "data-ads-portal-id": 5494710,
+          //     "data-ads-env": "prod",
+          //     "data-loader": "hs-scriptloader",
+          //     "data-hsjs-portal": 5494710,
+          //     "data-hsjs-env": "prod",
+          //     "data-hsjs-hublet": "na1"
+          // });
+          
+          `}</Script>
         </body>
       </Html>
     );
