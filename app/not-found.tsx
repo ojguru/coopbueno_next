c9bfa2240e1b3fc0f20ca19b1de78371554b41d3
Osiris Pena <osiris.pena@conuco.do>
2024-10-06 22:36:21 -0400

    Next 13: Not found

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./not-found.module.scss";
+
+const Page = () => {
+  return (
+    <>
+      <section className={styles.section}>
+        <h1 className={styles.title}>Página no encontrada</h1>
+      </section>
+    </>
+  );
+};
+
+export default Page;
